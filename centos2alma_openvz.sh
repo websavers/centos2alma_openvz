43b7087b5a106ef8781bdab673747c057cc99611
Jordan Schelew <jordan@websavers.ca>
2024-11-25 17:13:50 -0400

    Fix xmlrpc-c package conflicts by removing them in --prepare

--- centos2alma_openvz.sh
+++ centos2alma_openvz.sh
@@ -105,6 +105,7 @@ function ct_prepare {
     vzctl exec $CTID rpm -e MariaDB-server MariaDB-client MariaDB-shared MariaDB-common MariaDB-compat --nodeps
     vzctl exec $CTID rpm -e python36-PyYAML --nodeps
     vzctl exec $CTID rpm -e fail2ban --nodeps
+    vzctl exec $CTID rpm -e xmlrpc-c xmlrpc-c-c++ --nodeps
     # Plesk fail2ban dependencies:
     vzctl exec $CTID rpm -e python-inotify --nodeps
     vzctl exec $CTID rpm -e python2-inotify --nodeps
